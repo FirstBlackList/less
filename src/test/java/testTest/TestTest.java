47058fbe50ba4cc9002dc49ae35891de89dd9acf
BlackList <azokblack@gmail.com>
2023-11-03 20:47:01 +0400

    add ci1

---
+++ src/test/java/testTest/TestTest.java
@@ -0,0 +1,22 @@
+package testTest;
+
+import org.junit.jupiter.api.Test;
+
+public class TestTest {
+
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+
+    @Test
+    public void test3() {
+
+    }
+}
